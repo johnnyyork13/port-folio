bf8ad33b7217c29f8f6ee34d1ec1a884a7aee6f9
johnnyyork13 <johnnyyork13@hotmail.com>
2024-01-12 21:10:08 -0500

    add bulletin board styling

--- src/components/Bulletin.jsx
+++ src/components/Bulletin.jsx
@@ -6,7 +6,31 @@ export default function Bulletin(props) {
 
     return (
         <div className="bulletin-container">
-            BULLETIN
+            <p className="bulletin-header">Useful Information</p>
+            <div className="bulletin">
+                <div className="bulletin-section bulletin-left">
+                    <div className="bulletin-pins-container bulletin-pins-container-left">
+                        <div className="bulletin-pin-left"></div>
+                        <div className="bulletin-pin-left"></div>
+                    </div>
+                </div>
+                <div className="bulletin-section bulletin-top-right">
+                    <div className="bulletin-pins-container bullet-pins-container-top-right">
+                        <div className="bulletin-pin-top-right"></div>
+                        <div className="bulletin-pin-top-right"></div>
+                    </div>
+                </div>
+                <div className="bulletin-section bulletin-bottom-right">
+                    <div className="bulletin-pins-container bullet-pins-container-bottom-right">
+                        <div className="bulletin-pin-bottom-right"></div>
+                        <div className="bulletin-pin-bottom-right"></div>
+                    </div>
+                </div>
+            </div>
+            <button 
+                className="close-bulletin-container-btn"
+                onClick={() => props.setPage("")}
+            >Close</button>
         </div>
     )
 }
