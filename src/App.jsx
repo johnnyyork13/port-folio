84eb903ce9d21279e9692c176fcd929bcc58f8c6
johnnyyork13 <johnnyyork13@hotmail.com>
2024-01-31 20:00:44 -0500

    add reset stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {v4 as uuidv4} from 'uuid';
+import './styles/reset.css';
 import './styles/App.css';
 import './styles/queries.css';
 import bubble from './assets/bubble.png';
